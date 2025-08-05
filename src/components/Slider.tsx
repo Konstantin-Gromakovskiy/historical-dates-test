1c2072855e69280ca111f9081634512a67af35e8
Konstantin <k.gromakovskiy@yandex.ru>
2025-08-05 17:11:29 +0300

    feat: add pagination buttons

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,13 @@
 import { FC, HTMLAttributes } from 'react'
 import type { HistoricalEvent } from '@/__fixtures__/mockData'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, FreeMode } from 'swiper/modules'
+import { Navigation, FreeMode, Pagination } from 'swiper/modules'
 import './slider.module.scss'
 import 'swiper/scss/navigation'
 import 'swiper/scss'
+import 'swiper/scss/pagination'
 import styles from './slider.module.scss'
+import CircleButton from '@/shared/ui/CircleButton'
 
 interface SliderProps extends HTMLAttributes<HTMLDivElement> {
   historicalEvents: HistoricalEvent[]
@@ -13,15 +15,32 @@ interface SliderProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Slider: FC<SliderProps> = ({ historicalEvents, activeItemIndex, className }) => {
+  const currentItem = (activeItemIndex + 1).toString().padStart(2, '0')
+  // TODO: на данный момент приходит не то количество евентов
+  // Нужно сделать, чтобы отображалось количество временных отрезко а не евентов
+  const allItems = historicalEvents.length.toString().padStart(2, '0')
+
   return (
     <div className={`${styles.container} ${className ? className : ''}`}>
+      <div className={styles.paginationContainer}>
+        <div className={styles.pagination}>
+          <span>{currentItem}</span>
+          /
+          <span>{allItems}</span>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <CircleButton onClick={() => {}} />
+          <CircleButton disabled onClick={() => {}} />
+        </div>
+      </div>
       <Swiper
-        modules={[Navigation, FreeMode]}
+        modules={[Navigation, FreeMode, Pagination]}
         navigation
         initialSlide={activeItemIndex}
         className={styles.swiper}
         slidesPerView="auto"
         freeMode={true}
+        pagination={true}
       >
         {historicalEvents.map((event, i) => (
           <SwiperSlide
---
+++ src/shared/ui/CircleButton.tsx
@@ -0,0 +1,13 @@
+import { ButtonHTMLAttributes, FC } from 'react'
+import styles from './CircleButton.module.scss'
+// import ArrowIcon from '@/assets/icons/arrow.svg'
+
+// TODO: разобраться с имортом кнопки и сделать стрелку нужного цвета
+const CircleButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ disabled, onClick, className }) => {
+  return (
+    <button type="button" disabled={disabled} onClick={onClick} className={`${styles.button} ${className ? className : ''}`}>
+    </button>
+  )
+}
+
+export default CircleButton
